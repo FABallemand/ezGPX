64a600953dcff537693bb48f39542027305eebec
FABallemand <allemand.fabien@orange.fr>
2023-09-01 14:29:13 +0200

    #1 Work in progress: -Documentation test
    [ci skip]

--- ezgpx/utils/algorithms.py
+++ ezgpx/utils/algorithms.py
@@ -8,13 +8,13 @@ def ramer_douglas_peucker(points: list, epsilon: float = degrees(2/EARTH_RADIUS)
     """
     Simplify a curve using the Ramer-Douglas-Peucker algorithm.
     Source: https://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm
-    
-    Args:
-        points (list[TrackPoint]): List of points defining the curve.
-        epsilon (float, optional): _description_. Defaults to degrees(2/EARTH_RADIUS) (ie: the angle corresponding to a distance of 2 meters at the surface of the earth).
 
-    Returns:
-        list[TrackPoint]: List of points defining the simplified curve.
+    :param points: _description_
+    :type points: list[TrackPoint]
+    :param epsilon: Ramer-Douglas-Peucker threshold distance (higher value means more simplifications), defaults to degrees(2/EARTH_RADIUS) (ie: the angle corresponding to a distance of 2 meters at the surface of the earth).
+    :type epsilon: float, optional
+    :return: List of points defining the simplified curve.
+    :rtype: list[TrackPoint]
     """
     # Find the point with the maximum distance
     d_max = 0
